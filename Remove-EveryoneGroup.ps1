882990a52144b0107fbc4b6660dcd51e6cd6986d
Colin Hearn <117847247+Osclot@users.noreply.github.com>
2024-06-05 14:03:32 -0400

    Update Remove-EveryoneGroup.ps1

--- Remove-EveryoneGroup.ps1
+++ Remove-EveryoneGroup.ps1
@@ -1,89 +1,174 @@
 <#PSScriptInfo
 
-.VERSION 1.1.7
+.VERSION 1.0.3
 
-.GUID a88953e9-bba3-4eed-80a8-5bbd2597c97a
+.GUID abaa749f-9a32-4c17-8835-cc0ea82b8d6f
 
 .AUTHOR Colin Hearn
 
 .COMPANYNAME Leidos
 
-.COPYRIGHT 
+.COPYRIGHT
 
-.TAGS 
+.TAGS
 
-.LICENSEURI 
+.LICENSEURI
 
-.PROJECTURI 
+.PROJECTURI
 
-.ICONURI 
+.ICONURI
 
 .EXTERNALMODULEDEPENDENCIES 
 
-.REQUIREDSCRIPTS 
+.REQUIREDSCRIPTS
 
-.EXTERNALSCRIPTDEPENDENCIES 
+.EXTERNALSCRIPTDEPENDENCIES
 
 .RELEASENOTES
-1.0.5 - Fixed issue with $endreport collection that caused improper output and memory usage.  
-1.0.6 - Fixed error that caused $OutputPath parameter to be mandatory. 
-1.0.7 - Converted all Arrays to ArrayLists for performance increase. 
-1.0.8 - Automated and removed the -Full switch.
-1.0.9 - Changed default behavior to hide progress. -ShowProgress will now display progress.
-1.1.0 - Rewrite of entire script to improve performance and manageability. Changes to remove NTFSSecurity pending.
-1.1.1 - Removed commented-out variables. Added logic to determine $UNCPath using regular expressions. 
-1.1.3 - Added runspaces to improve performance.
-1.1.5 - Added synchronized hash tables for progress tracking. 
-1.1.6 - Updated max thread limits to vary per machine. 
-#>
 
-#Requires -Module NTFSSecurity
+
+.PRIVATEDATA
+
+#>
 
 <# 
 
 .DESCRIPTION 
- Removes the Everyone group from specified home directories. Removes inheritance and adds the Administrator group if required. 
+ Finds servers in each domain that are being backed up to SnapCenter or SnapManager. 
 
 #> 
-
 Param(
-    # Home directory folder. Example: $UNCName = "\\[ServerName]\[DirectoryPath]$\"
-    [Parameter( Mandatory )]
+    [Parameter()]
     [String]
-    $UNCName
+    $CsvPath = "C:\test\ProtectedServers.csv"
 )
-<#
+$allServers = @{
+    "East" = @{
+        "Domain" = 
+            "nadsusea.nads.navy.mil"
+        "Locality" = 
+            "East Coast"
+        "ServerLocators" = @(
+            "NAEACHRLSQ*",                
+            "NAEACRANSQ*",
+            "NAEAJAXSSQ*",
+            "NAEAMECHSQ*",
+            "NAEANRFKSQ*",
+            "NAEAPAXRSQ*",
+            "NAEAWNYDSQ*", 
+            "NAEACHRLX*",                
+            "NAEACRANX*",
+            "NAEAJAXSX*",
+            "NAEAMECHX*",
+            "NAEANRFKX*",
+            "NAEAPAXRX*",
+            "NAEAWNYDX*"
+            )
+    }
+    "West" = @{
+        "Domain" = 
+            "nadsuswe.nads.navy.mil"
+        "Locality" = 
+            "West Coast"
+        "ServerLocators" =
+            "NAWEBREMSQ*",
+            "NAWECHLKSQ*",
+            "NAWESDNISQ*",
+            "NAWESPSCSQ*",
+            "NAWEPRLHSQ*",
+            "NAWEBREMX*",
+            "NAWECHLKX*",
+            "NAWESDNIX*",
+            "NAWESPSCX*",
+            "NAWEPRLHX*"
+    }
+    "PACOM" = @{
+        "Domain" = 
+            "pads.pacom.mil"
+        "Locality" = 
+            "Pads"
+        "ServerLocators" = @(
+            "PADSSPRLHSQ*",
+            "PADSPRLHX*"
+            )
+    }
+    "NMCI" = @{
+        "Domain" = 
+            "nmci-isf.com"
+        "Locality" = 
+            "NMCI"
+        "ServerLocators" = @(
+            "NMCI*SQ*",
+            "NMCI*X*"
+            )
+    } 
+}
 
-$adminCount      : Number of folders missing Administrators group.
-$acl             : ACL Object of the current $folder in the loop. 
-$changeCount     : Number of folders modified
-$dir             : Each item in $UNCDirectories
-$everyCount      : Number of folders with Everyone Group present.
-$everyoneGroup   : Used to check and remove Everyone Group if present. Count = 0 if absent.
-$folder          : Current $UNCChildren item in the loop.
-$folderMod       : True if current folder ACL was changed.
-$inherCount      : Number of folders with inheritance enabled.
-$UNCChildren     : Folders contained within $UNCName.
-$UNCDirectories  : Child items of ..\home\
-$UNCName         : Directory to be checked.
-
-#>
 $threadCount = (Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors
-$SharedChanges = [HashTable]::Synchronized(@{})
-$SharedChanges.ChangesMade = 0
-$SharedChanges.FoldersComplete = 0
-
+$FoundServers = [HashTable]::Synchronized(@{})
+$FoundServers.Complete = 0
 
 $RunspacePool = [runspacefactory]::CreateRunspacePool(1,$threadCount)
 $RunspacePool.ApartmentState = "MTA"
 $RunspacePool.Open()
 [System.Collections.ArrayList]$RunspaceCollection = @()
+$ScriptBlock = {
+    param(
+        [string]$ServerLocator,
+        [Microsoft.ActiveDirectory.Management.ADDomainController]$dc,
+        [string]$Locality, 
+        [hashtable]$FoundServers
+        )
+    Write-Verbose "Searching for servers with locator $ServerLocator in domain $($dc.Domain)."
+    # SnapDrive SQ = SnapManagerService
+    # SnapCenter SQ = SCWPluginService + SCSQLPluginService
+    # Exchange = SCWPluginService
+    Import-Module ActiveDirectory
+    $serverList = (Get-ADComputer -Filter "Name -like $ServerLocator" -Server $dc.HostName[0]).DNSHostName
+    if(-not $serverList){
+        Write-Verbose "No servers found for locator $ServerLocator ."
+    }
+    foreach($foundServer in $serverList){
+        Write-Verbose "Processing server $foundServer"
+        $sdSvc = (Get-Service -ComputerName $foundServer -ServiceName SnapManagerService -ErrorAction SilentlyContinue).StartType
+        $scSvc = (Get-Service -ComputerName $foundServer -ServiceName SCSQLPluginService -ErrorAction SilentlyContinue).StartType
+        $exSvc = (Get-Service -ComputerName $foundServer -ServiceName SCWPluginService -ErrorAction SilentlyContinue).StartType
+        if($sdSvc -eq 'Automatic'){
+            $ProtectionType = "SnapDrive SQL"
+            $SvcAccount = (Get-WmiObject win32_service -ComputerName $foundServer -Filter "Name='SnapManagerService'").StartName
+        }
+        elseif($scSvc -eq 'Automatic'){
+            $ProtectionType = "SnapCenter SQL"
+            $SvcAccount = (Get-WmiObject win32_service -ComputerName $foundServer -Filter "Name='SCSQLPluginService'").StartName
+        }
+        elseif($exSvc -eq 'Automatic'){
+            $ProtectionType = "SnapCenter Exchange"
+            $SvcAccount = (Get-WmiObject win32_service -ComputerName $foundServer -Filter "Name='SCWPluginService'").StartName
+        }
+        else{
+            Continue
+        }
+        Write-Verbose "Adding server $foundServer to the list with protection type $ProtectionType"
+        $FoundServers[$Locality][$foundServer] += @{
+            "Protection Type" = $ProtectionType
+            "Service Account" = $SvcAccount
+            "Domain" = $dc.Domain
+            "Site" = $dc.Site
+        }
+        $FoundServers.Complete++
+    }
+}
 $RunspaceBlock = {
-    param($UNCChildren)
-    $t = ($UNCChildren | Measure-Object).Count
-    $i = 0
-    foreach($folder in $UNCChildren){
-        $Instance = [powershell]::Create().AddScript($ScriptBlock).AddArgument($folder).AddArgument($SharedChanges)
+    param(
+        [string]$Domain, 
+        [string]$Locality, 
+        [array]$ServerLocators, 
+        [hashtable]$FoundServers
+        )
+    Write-Verbose "Discovering domain controller for domain $Domain"
+    $dc = Get-ADDomainController -DomainName $Domain -Discover -NextClosestSite
+    foreach($ServerLocator in $ServerLocators){
+        $Instance = [powershell]::Create().AddScript($ScriptBlock).AddArgument($ServerLocator).AddArgument($dc).AddArgument($Locality).AddArgument($FoundServers)
         $Instance.RunspacePool = $RunspacePool
         $RunspaceCollection.Add([PSCustomObject]@{
         Results  = $Instance.BeginInvoke()
@@ -91,8 +176,7 @@ $RunspaceBlock = {
         }) | Out-Null
     }
     do {
-        $i = $SharedChanges.FoldersComplete
-        Write-Progress -Activity "All folders loaded. Checking/Updating." -Status "$i/$t Folders Complete."
+        Write-Progress -Activity "All servers loaded for $Locality." -Status "$($FoundServers.Complete) Servers Complete."
         Start-Sleep -Seconds 1
     }
     while ($RunspaceCollection.Results.IsCompleted -contains $false)
@@ -103,65 +187,12 @@ $RunspaceBlock = {
         }
     }
 }
-$ScriptBlock = {
-    param($folder,$SharedChanges)
-    Import-Module NTFSSecurity
-    # Check access to folder. Add admin if needed.
-    try{
-        $acl = $folder.GetAccessControl()
-    }
-    catch{
-        Add-NTFSAccess $folder -Account BUILTIN\Administrators -AccessRights FullControl -AccessType Allow -InheritanceFlags ContainerInherit,ObjectInherit -PropagationFlags None
-        $acl = $folder.GetAccessControl()                
-    }
-    # Disable inheritance.
-    if($acl.access.IsInherited -eq $true){
-        $acl.SetAccessRuleProtection($true,$true)
-        $folder.SetAccessControl($acl)
-    }
-    # Check if Administrator Group was removed due to inheritance. Add if needed.
-    try{
-        $acl = $folder.GetAccessControl()
-    }
-    catch{
-        Add-NTFSAccess $folder -Account BUILTIN\Administrators -AccessRights FullControl -AccessType Allow -InheritanceFlags ContainerInherit,ObjectInherit -PropagationFlags None
-        $acl = $folder.GetAccessControl()
-    }
-    # Check if Everyone Group exists. Remove if needed.
-    $everyoneGroup = $acl.Access.Where({ $_.IdentityReference -match 'Everyone' })
-    if($everyoneGroup.count -ne 0){
-        $everyoneGroup | ForEach-Object { $acl.RemoveAccessRule($_) } | Out-Null
-        $folder.SetAccessControl($acl)
-        $folderMod = $true
-    }
-    if($folderMod){
-        $SharedChanges.ChangesMade++
-        $SharedChanges.FolderNames += $folder.Name + "`n"
-    }
-    $SharedChanges.FoldersComplete++
-}
-if ($UNCName -match "^\\\\[^\\]+\\c\$\\home$") {
-    # Get all subfolders under /home
-    $UNCDirectories = Get-ChildItem $UNCName -Directory | Sort-Object
-    foreach($dir in $UNCDirectories){
-        Write-Host "Retrieving child folders of $dir."
-        $UNCChildren = Get-ChildItem $dir.FullName -Directory | Sort-Object
-        &$RunspaceBlock -UNCChildren $UNCChildren | Out-Null
-        $SharedChanges.FolderNames
-        Write-Host "Folders Requiring Everyone Group Removal in $dir : ",$SharedChanges.ChangesMade
-        $SharedChanges.ChangesMade = 0
-        $SharedChanges.FoldersComplete = 0
-        $SharedChanges.Remove('FolderNames')
-    }
-} 
-else{
-    Write-Host "Retrieving child folders of $UNCName."
-    $UNCChildren = Get-ChildItem $UNCName -Directory | Sort-Object
-    &$RunspaceBlock -UNCChildren $UNCChildren | Out-Null
-    $SharedChanges.FolderNames
-    Write-Host "Folders Requiring Everyone Group Removal: ",$SharedChanges.ChangesMade
+
+Foreach($region in $AllServers.Keys){
+    $h = ($AllServers.$region.Value)
+    &$RunspaceBlock -Domain $h.Domain -ServerLocators $h.ServerLocators -Locality $h.Locality -FoundServers $FoundServers
 }
 
-
+$FoundServers | Export-Csv -Path $CsvPath
 $RunspacePool.Close()
 $RunspacePool.Dispose()
